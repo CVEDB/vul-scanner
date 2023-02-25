70da6dc05669d82235cd1b1c67607e945b5f1289
Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>
2023-02-24 22:09:42 -0800

    Create main.go
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>

---
+++ cmd/vul-scanner/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/CVEDB/vul-scanner/internal/output"
+	"github.com/CVEDB/vul-scanner/pkg/vulscanner"
+
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	version = "dev"
+	commit  = "n/a"
+	date    = "n/a"
+)
+
+func run(args []string, stdout, stderr io.Writer) int {
+	var r *output.Reporter
+
+	cli.VersionPrinter = func(ctx *cli.Context) {
+		r = output.NewReporter(ctx.App.Writer, ctx.App.ErrWriter, "")
+		r.PrintText(fmt.Sprintf("vul-scanner version: %s\ncommit: %s\nbuilt at: %s\n", ctx.App.Version, commit, date))
+	}
+
+	app := &cli.App{
+		Name:      "vul-scanner",
+		Version:   version,
+		Usage:     "scans various mediums for dependencies and matches it against the OSV database",
+		Suggest:   true,
+		Writer:    stdout,
+		ErrWriter: stderr,
+		Flags: []cli.Flag{
+			&cli.StringSliceFlag{
+				Name:      "docker",
+				Aliases:   []string{"D"},
+				Usage:     "scan docker image with this name",
+				TakesFile: false,
+			},
+			&cli.StringSliceFlag{
+				Name:      "lockfile",
+				Aliases:   []string{"L"},
+				Usage:     "scan package lockfile on this path",
+				TakesFile: true,
+			},
+			&cli.StringSliceFlag{
+				Name:      "sbom",
+				Aliases:   []string{"S"},
+				Usage:     "scan sbom file on this path",
+				TakesFile: true,
+			},
+			&cli.StringFlag{
+				Name:      "config",
+				Usage:     "set/override config file",
+				TakesFile: true,
+			},
+			&cli.StringFlag{
+				Name:    "format",
+				Aliases: []string{"f"},
+				Usage:   "sets the output format",
+				Value:   "table",
+				Action: func(context *cli.Context, s string) error {
+					switch s {
+					case
+						"table",
+						"json",
+						"markdown":
+						return nil
+					}
+
+					return fmt.Errorf("unsupported output format \"%s\" - must be one of: \"table\", \"json\", \"markdown\"", s)
+				},
+			},
+			&cli.BoolFlag{
+				Name:  "json",
+				Usage: "sets output to json (deprecated, use --format json instead)",
+			},
+			&cli.BoolFlag{
+				Name:  "skip-git",
+				Usage: "skip scanning git repositories",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name:    "recursive",
+				Aliases: []string{"r"},
+				Usage:   "check subdirectories",
+				Value:   false,
+			},
+			&cli.BoolFlag{
+				Name:  "no-ignore",
+				Usage: "also scan files that would be ignored by .gitignore",
+				Value: false,
+			},
+		},
+		ArgsUsage: "[directory1 directory2...]",
+		Action: func(context *cli.Context) error {
+			format := context.String("format")
+
+			if context.Bool("json") {
+				format = "json"
+			}
+
+			r = output.NewReporter(stdout, stderr, format)
+
+			vulnResult, err := osvscanner.DoScan(osvscanner.ScannerActions{
+				LockfilePaths:        context.StringSlice("lockfile"),
+				SBOMPaths:            context.StringSlice("sbom"),
+				DockerContainerNames: context.StringSlice("docker"),
+				Recursive:            context.Bool("recursive"),
+				SkipGit:              context.Bool("skip-git"),
+				NoIgnore:             context.Bool("no-ignore"),
+				ConfigOverridePath:   context.String("config"),
+				DirectoryPaths:       context.Args().Slice(),
+			}, r)
+
+			if errPrint := r.PrintResult(&vulnResult); errPrint != nil {
+				return fmt.Errorf("failed to write output: %w", errPrint)
+			}
+			//nolint:wrapcheck
+			return err
+		},
+	}
+
+	if err := app.Run(args); err != nil {
+		if r == nil {
+			r = output.NewReporter(stdout, stderr, "")
+		}
+		if errors.Is(err, osvscanner.VulnerabilitiesFoundErr) {
+			return 1
+		}
+
+		if errors.Is(err, osvscanner.NoPackagesFoundErr) {
+			r.PrintError("No package sources found, --help for usage information.\n")
+			return 128
+		}
+
+		r.PrintError(fmt.Sprintf("%v\n", err))
+	}
+
+	// if we've been told to print an error, and not already exited with
+	// a specific error code, then exit with a generic non-zero code
+	if r != nil && r.HasPrintedError() {
+		return 127
+	}
+
+	return 0
+}
+
+func main() {
+	os.Exit(run(os.Args, os.Stdout, os.Stderr))
+}
