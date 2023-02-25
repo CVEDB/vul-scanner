a4c7f10e71aead912da107063c31a3bcfa46adf9
Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>
2023-02-24 22:19:07 -0800

    Create cyclonedx.go
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>

---
+++ internal/sbom/cyclonedx.go
@@ -0,0 +1,55 @@
+package sbom
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/CycloneDX/cyclonedx-go"
+)
+
+type CycloneDX struct{}
+
+var (
+	cycloneDXTypes = []cyclonedx.BOMFileFormat{
+		cyclonedx.BOMFileFormatJSON,
+		cyclonedx.BOMFileFormatXML,
+	}
+)
+
+func (c *CycloneDX) Name() string {
+	return "CycloneDX"
+}
+
+func (c *CycloneDX) enumeratePackages(bom *cyclonedx.BOM, callback func(Identifier) error) error {
+	for _, component := range *bom.Components {
+		if component.PackageURL != "" {
+			err := callback(Identifier{
+				PURL: component.PackageURL,
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (c *CycloneDX) GetPackages(r io.ReadSeeker, callback func(Identifier) error) error {
+	var bom cyclonedx.BOM
+
+	for _, formatType := range cycloneDXTypes {
+		_, err := r.Seek(0, io.SeekStart)
+		if err != nil {
+			return fmt.Errorf("failed to seek to start of file: %w", err)
+		}
+		decoder := cyclonedx.NewBOMDecoder(r, formatType)
+		err = decoder.Decode(&bom)
+		if err == nil && (bom.BOMFormat == "CycloneDX" || strings.HasPrefix(bom.XMLNS, "http://cyclonedx.org/schema/bom")) {
+			return c.enumeratePackages(&bom, callback)
+		}
+	}
+
+	return ErrInvalidFormat
+}
